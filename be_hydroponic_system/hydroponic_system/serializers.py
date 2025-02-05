e95a54698749a3f72b2cd4ff8a83d164e9f31d9d
Kajetan Janusz <kajtek.k.janusz@gmail.com>
2025-02-05 11:23:55 +0100

    managing hydroponic system

---
+++ be_hydroponic_system/hydroponic_system/serializers.py
@@ -0,0 +1,31 @@
+from rest_framework import serializers
+from .models import CustomUser, HydroponicSystem
+
+class CustomUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ('id', 'username', 'email', 'phone_number')
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = CustomUser.objects.create_user(**validated_data)
+        return user
+    
+
+class HydroponicSystemSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+
+    class Meta:
+        model = HydroponicSystem
+        fields = ['name', 'description', 'system_type', 'plant_type', 'ph_level', 'ec_level', 'created_at', 'updated_at', 'owner']
+
+    def validate_ph_level(self, value):
+        if value < 0 or value > 14:
+            raise serializers.ValidationError("pH level must be between 0 and 14.")
+        return value
+
+    def validate_ec_level(self, value):
+        if value < 0:
+            raise serializers.ValidationError("EC level cannot be negative.")
+        return value
+
